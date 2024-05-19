fd09872d67acd6b6ebe53c091d962f455339d5cb
miwanoff <marina.iva.noffa@gmail.com>
2024-05-19 14:50:56 +0300

    Add dynamic Search form

--- wp-content/themes/bootkit/searchform.php
+++ wp-content/themes/bootkit/searchform.php
@@ -1,12 +1,16 @@
-        <!-- Search Widget -->
-        <div class="card mb-4">
-            <h5 class="card-header">Search</h5>
-            <div class="card-body">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="inpug-group-append">
-                        <button class="btn btn-secondary" type="button">Go!</button>
-                    </span>
-                </div>
+<?php $unique_id = esc_attr(uniqid('search-form-'));?>
+<!-- Search Widget -->
+<div class="card mb-4">
+    <h5 class="card-header"><?php _e('Search');?></h5>
+    <div class="card-body">
+        <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+            <div class="input-group">
+                <input type="search" id="<?php echo $unique_id; ?>" name="s" value="<?php echo get_search_query() ?>"
+                    class="form-control" placeholder="<?php _e('Search');?>">
+                <span class="input-group-append">
+                    <button class="btn btn-secondary" type="submit"><?php _e('Search');?></button>
+                </span>
             </div>
-        </div>
+        </form>
+    </div>
+</div>
