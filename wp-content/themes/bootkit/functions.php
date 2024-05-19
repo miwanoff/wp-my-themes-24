e53844da377c226eba8d5a91e69497357b3334a7
miwanoff <marina.iva.noffa@gmail.com>
2024-05-19 15:46:11 +0300

    Add posts nav links

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -18,3 +18,12 @@ add_action('after_setup_theme', 'bootkit_setup_theme');
 add_action('widgets_init', 'bootkit_widgets');
 
 // Shortcodes
+
+
+
+
+// add_filter('next_posts_link_attributes', 'posts_link_attributes');
+// add_filter('previous_posts_link_attributes', 'posts_link_attributes');
+// function posts_link_attributes() {
+//   return 'class="page-link"';
+// }
