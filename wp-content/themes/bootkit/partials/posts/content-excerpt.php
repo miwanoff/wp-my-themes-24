7200a2c0b7c422d24dd366d7ad00ddc300387e25
miwanoff <marina.iva.noffa@gmail.com>
2024-05-19 15:19:43 +0300

    Add post_thumbnail

--- wp-content/themes/bootkit/partials/posts/content-excerpt.php
+++ wp-content/themes/bootkit/partials/posts/content-excerpt.php
@@ -1,6 +1,12 @@
             <!-- Blog Post -->
             <div class="card mb-4">
-                <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+                <!-- <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> -->
+                <?php
+if (has_post_thumbnail()) {
+
+    the_post_thumbnail("full", ["class" => "card-img-top"]);
+
+}?>
                 <div class="card-body">
                     <h2 class="card-title">Post Title</h2>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
