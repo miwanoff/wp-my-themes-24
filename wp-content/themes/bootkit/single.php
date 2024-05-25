8a43da76f7174cb25614bf6441c58c0cbd81c896
miwanoff <marina.iva.noffa@gmail.com>
2024-05-25 15:16:37 +0300

    Add tags

--- wp-content/themes/bootkit/single.php
+++ wp-content/themes/bootkit/single.php
@@ -50,29 +50,13 @@ if (has_post_thumbnail()) {
         ?>
             <hr>
             <!-- Post Content -->
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut,
-                error quam sapiente nemo saepe quibusdam є excepturi nam quia corporis escollendi eos magni recusandae
-                laborum minus inventore?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste
-                ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus,
-                voluptatibus.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde
-                eius illum consequuntur не dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim,
-                iure!</p>
-            <blockquote class="blockquote">
-                <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
-                </p>
-                <footer class="blockquote-footer">Someone famous in
-                    <cite title="Source Title">Source Title</cite>
-                </footer>
-            </blockquote>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, at quas placeat
-                totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, Dolor quis. Sunt, ut, explicabo, aliquam
-                tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae?
-                Qui, necessitatibus, est!</p>
+            <?php the_content();?>
             <hr>
-
+            <?php if (has_tag()) {?>
+            <!-- Tag cloud -->
+            <?php the_tags('', ', ');?>
+            <hr>
+            <?php }?>
             <!-- Comments Form -->
 
             <div class="card my-4">
