a3b09f332bda2e9b6452fd7f7809cee76eefc873
miwanoff <marina.iva.noffa@gmail.com>
2024-06-01 15:33:54 +0300

    Add time_shortcode

--- wp-content/plugins/time-to-post-title.php
+++ wp-content/plugins/time-to-post-title.php
@@ -13,3 +13,11 @@ function time_to_post_content($content)
     return '<div class="time">' . __('Time of post creation: ') . get_the_time() . " " . get_the_date() . '</div>' . $content;
 }
 add_filter('the_content', 'time_to_post_content');
+
+// Shortcodes
+
+function time_shortcode($atts)
+{
+    return '<div class="time">' . __('Time of post creation: ') . get_the_time() . " " . get_the_date() . '</div>';
+}
+add_shortcode('time_to_post', 'time_shortcode');
