6037fa30ca82cd383076971ac480d2cb8cfd11c6
miwanoff <marina.iva.noffa@gmail.com>
2024-06-01 15:40:22 +0300

    Add breadcrumbs_shortcode

--- wp-content/plugins/breadcrumbs/breadcrumbs.php
+++ wp-content/plugins/breadcrumbs/breadcrumbs.php
@@ -36,3 +36,47 @@ function breadcrumbs()
     echo '</ol>';
 }
 //add_action('wp_head', 'breadcrumbs');
+
+function breadcrumbs_for_shortcode()
+{
+    $breadcrumbs = '<ol class="breadcrumb">';
+    if (!is_front_page()) {
+        $breadcrumbs .= '<li class="breadcrumb-item"><a href="';
+        $breadcrumbs .= get_option('home');
+        $breadcrumbs .= '">' . __("Home") . '</a></li>';
+        if (is_category()) {
+            $breadcrumbs .= '<li class="breadcrumb-item">';
+            $categorie = get_category(get_query_var('cat'));
+            $breadcrumbs .= '<a href="' . get_category_link($categorie->term_id) . '">' . $categorie->name . '</a> / ';
+            $breadcrumbs = trim($breadcrumbs, ' / ');
+        } elseif (is_single()) {
+            $breadcrumbs .= '<li class="breadcrumb-item">';
+            $categories = get_the_category();
+            if ($categories) {
+                foreach ($categories as $category) {
+                    $breadcrumbs .= '<a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a> / ';
+                }
+                $breadcrumbs = trim($breadcrumbs, ' / ');
+            }
+            $breadcrumbs .= '</li>';
+            if (is_single()) {
+                $breadcrumbs .= '<li class="breadcrumb-item active">' . get_the_title();
+                $breadcrumbs .= '</li>';
+            }
+        } elseif (is_page()) {
+            $breadcrumbs .= '<li class="breadcrumb-item active">';
+            $breadcrumbs .= get_the_title();
+            $breadcrumbs .= '</li>';
+        }
+    } else {
+        $breadcrumbs .= __('<li class="breadcrumb-item">' . __("Home") . '</li>');
+    }
+    $breadcrumbs .= '</ol>';
+    return $breadcrumbs;
+}
+
+function breadcrumbs_shortcode($atts)
+{
+    return breadcrumbs_for_shortcode();
+}
+add_shortcode('breadcrumbs', 'breadcrumbs_shortcode');
